9a0686c537f9f1cfe50c67efcb344ed559540040
Gethsun Misesi <gethsun09@gmail.com>
2025-03-14 14:50:06 +0300

    Update HeroSection.jsx

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -56,7 +56,7 @@ const HeroSection = () => {
               gutterBottom
               sx={{
                 fontWeight: 900,
-                fontFamily: 'Chonburi',
+                fontFamily: '"Chonburi", serif',
                 fontSize: { xs: '2.5rem', md: '4rem' },
               }}
             >

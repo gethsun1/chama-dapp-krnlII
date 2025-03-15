633de8a3876be943a1df01c3066f73043618811a
Gethsun1 <gethsun09@gmail.com>
2025-03-16 00:15:48 +0300

    smart contract deploy script added

---
+++ src/scripts/deploy.js
@@ -0,0 +1,26 @@
+const hre = require("hardhat");
+
+async function main() {
+  const [deployer] = await hre.ethers.getSigners();
+  console.log("Deploying contract with account:", deployer.address);
+  
+  const balance = await hre.ethers.provider.getBalance(deployer.address);
+  console.log("Account balance:", balance.toString());
+  
+  const ChamaFactory = await hre.ethers.getContractFactory("ChamaFactory");
+  const contract = await ChamaFactory.deploy();
+  
+  // Wait for the deployment to be mined
+  await contract.waitForDeployment();
+  
+
+  console.log("ChamaFactory deployed to:", contract.target);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });                                                                                                                                                                                                                                                                                                                                                                                                                                                         
+git
